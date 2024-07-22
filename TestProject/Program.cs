b5dc2fdff0e81033c6ffba6682764c7f163f8277
Justin Howe <jwhowe02@proton.me>
2024-07-21 22:49:41 -0700

    Array fiddlin' - dump array vals that start with 'B'

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] fraudulentOrderIDs = [
+    "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"
+];
+
+Console.WriteLine($"{fraudulentOrderIDs.Length} fraudulent order listing{(fraudulentOrderIDs.Length > 1 ? "s" : "")}:");
+
+// Display only the fraudulent order IDs that start with 'B'.
+foreach (string fraudulentOrderID in fraudulentOrderIDs) {
+    if (fraudulentOrderID.StartsWith('B')) {
+        Console.WriteLine($"Order ID: {fraudulentOrderID}");
+    }
+}
