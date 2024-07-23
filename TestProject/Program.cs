b24eb755dfb227b3286474ffed046abb620495f7
Justin Howe <jwhowe02@proton.me>
2024-07-23 13:40:29 -0700

    Implement basic string parser

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,28 +1,63 @@
-﻿string[] rolesAllowed = ["Administrator", "Manager", "User"];
-string? input;
+﻿using System.Diagnostics;
 
-Console.WriteLine("Please enter one of the following roles (Ctrl-C to quit):");
-int roleIndex = 0;
+void easyShortSolution() {
+    string[] testStrings = [
+        "     I like pizza. I like roast chicken. I like salad.",
+        "   I like all three!"
+    ];
 
-foreach (string role in rolesAllowed) {
-    Console.WriteLine($"{++roleIndex}. {role}");
+    foreach (string pizzaPrefs in testStrings) {
+        // For the first string, parse out the sentences.
+        string[] subStrings = pizzaPrefs.Split(". ");
+
+        foreach (string substr in subStrings) {
+            Console.WriteLine(substr.TrimStart().TrimEnd('.'));
+        }
+    }
 }
 
-bool invalidRole;
+void longConvolutedSolution() {
+    string[] testStrings = [
+        "     I like pizza. I like roast chicken. I like salad.",
+        "   I like all three!"
+    ];
 
-do {
-    input = Console.ReadLine();
+    int periodLocation;
 
-    if (input == null) {
-        break;
+    foreach (string myString in testStrings) {
+        string subSentence, sentenceFragment = myString;
+
+        // Get the next period location
+        periodLocation = myString.IndexOf('.');
+
+        while (periodLocation != -1) {
+            subSentence = sentenceFragment.Remove(periodLocation).TrimStart();
+            sentenceFragment = sentenceFragment[(periodLocation + 1)..];
+            periodLocation = sentenceFragment.IndexOf('.');
+
+            Console.WriteLine(subSentence);
+        }
+
+        Console.WriteLine(sentenceFragment.Trim());
     }
-
-    invalidRole = !rolesAllowed.Contains<string>(input);
-    if (invalidRole) {
-        Console.WriteLine($"The role you entered, {input}, is not a valid role. Please enter one of the roles listed to continue.");
-    }
-} while (invalidRole);
-
-if (input != null) {
-    Console.WriteLine($"You selected the {input} role. Thank you!");
 }
+
+// Profile shorter, more straightforward solution (which is also easier to read)
+Stopwatch stopper = Stopwatch.StartNew();
+
+Console.WriteLine("Easy, short pizza menu parsing solution:");
+easyShortSolution();
+
+stopper.Stop();
+long endTime = stopper.ElapsedMilliseconds;
+Console.WriteLine($"Execution time: {endTime}ms.\n");
+
+// Profile longer, more detailed solution
+stopper.Restart();
+Console.WriteLine("Longer pizza ingredient menu solution:");
+longConvolutedSolution();
+
+stopper.Stop();
+endTime = stopper.ElapsedMilliseconds;
+Console.WriteLine($"Execution time: {endTime}ms.");
+
